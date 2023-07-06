33fb612c8aa2c0375fbd7aee18e4c9300705b1e9
Olayinka <olayinka4grace@gmail.com>
2023-07-06 16:02:10 +0100

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Olayinka Grace",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "olayinka4grace@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
